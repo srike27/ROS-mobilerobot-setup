50db57c5927c403d13bacc3f1e92d399573e1b2f
srike27 <srike27@gmail.com>
2018-05-21 12:27:29 +0530

    added the package and explained stuff more

--- odom_pub.cpp
+++ odom_pub.cpp
@@ -5,6 +5,9 @@
 */
 
 #include <ros/ros.h>
+/*
+Since we're going to be publishing both a transform from the "odom" coordinate frame to the "base_link" coordinate frame and a nav_msgs/Odometry message, we need to include the relevant header files. 
+*/
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 
@@ -12,13 +15,18 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "odometry_publisher");
 
   ros::NodeHandle n;
+/*
+We need to create both a ros::Publisher and a tf::TransformBroadcaster to be able to send messages out using ROS and tf respectively.
+*/
   ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
   tf::TransformBroadcaster odom_broadcaster;
-
+//We'll assume that the robot starts at the origin of the "odom" coordinate frame initially.
   double x = 0.0;
   double y = 0.0;
   double th = 0.0;
-
+/*
+Here we'll set some velocities that will cause the "base_link" frame to move in the "odom" frame at a rate of 0.1m/s in the x direction, -0.1m/s in the y direction, and 0.1rad/s in the th direction. This will more or less cause our fake robot to drive in a circle. 
+*/
   double vx = 0.1;
   double vy = -0.1;
   double vth = 0.1;
@@ -26,7 +34,9 @@ int main(int argc, char** argv){
   ros::Time current_time, last_time;
   current_time = ros::Time::now();
   last_time = ros::Time::now();
-
+/*
+We'll publish odometry information at a rate of 1Hz in this example to make introspection easy, most systems will want to publish odometry at a much higher rate. 
+*/
   ros::Rate r(1.0);
   while(n.ok()){
 
@@ -42,7 +52,9 @@ int main(int argc, char** argv){
     x += delta_x;
     y += delta_y;
     th += delta_th;
-
+/*
+Here we'll update our odometry information based on the constant velocities we set. A real odometry system would, of course, integrate computed velocities instead.
+*/
     //since all odometry is 6DOF we'll need a quaternion created from yaw
     geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
 
